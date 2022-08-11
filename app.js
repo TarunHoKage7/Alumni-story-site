ce1c1aa064eb69de533183197929e7a3782c1ffc
UserTarun <saikalyantarun2001@gmail.com>
2022-08-11 21:53:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+
+//load config
+dotenv.config({ path:'./config/config.env' })
+
+connectDB()
+
+const app = express()
+
+//logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
+//handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', ".hbs")
+
+
+//Routes
+
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+//Login/Landing page
+//@route GET/
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+
+// Dashboard
+//@route GET /dashboard
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+
+module.exports = router
