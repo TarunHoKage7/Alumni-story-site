27b031bc70bcfb633fc01e5f36e2f318432cd87a
UserTarun <saikalyantarun2001@gmail.com>
2022-08-17 22:55:37 +0530

    logout and protecting routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
 
 //Login/Landing page
 //@route GET/
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
@@ -14,7 +16,8 @@ router.get('/', (req, res) => {
 // Dashboard
 //@route GET /dashboard
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
+    console.log(req.user)
     res.render('dashboard')
 })
 
